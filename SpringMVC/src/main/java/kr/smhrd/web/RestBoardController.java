23e620ed6635e09433da670d6b6bff53ca118ce3
Kimsunghun <rlatjdgns432@gmail.com>
2021-08-30 17:50:27 +0900

    - RestController 구성

---
+++ SpringMVC/src/main/java/kr/smhrd/web/RestBoardController.java
@@ -0,0 +1,37 @@
+package kr.smhrd.web;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.smhrd.dao.BoardMapper;
+import kr.smhrd.dao.BoardVO;
+
+// @Controller -> @RestController로 변경
+@RestController // RestController : JSON dataformat으로 응답할 때 사용 -> OPEN API(URL로 요청 -> 서버가 데이터 제공 : XML,JSON)
+// XML이 태그를 기반으로 하다보니 데이터량이 비대해짐 -> JSON을 통해 해결
+public class RestBoardController {
+
+	@Inject
+	private BoardMapper mapper;
+	
+	@RequestMapping("list.do")
+	public List<BoardVO> boardList(Model model){
+		List<BoardVO> list = mapper.boardList();
+		return list; // jsp주소가 아닌 객체를 보내버림 -> JSON으로 변환됨 (스프링프레임워크가 내부적으로 변환)
+		// Object -> JSON변환 -> JSON타입으로 응답
+		// [
+		// {"idx":1, "title":"스프링1", "contents":"스프링1","count":0, "writer":"관리자", "indate":"2021-08-27"},
+		// {"idx":2, "title":"스프링2", "contents":"스프링2","count":0, "writer":"관리자", "indate":"2021-08-27"},
+		// {"idx":3, "title":"스프링3", "contents":"스프링3","count":0, "writer":"관리자", "indate":"2021-08-27"},
+		// {"idx":4, "title":"스프링4", "contents":"스프링4","count":0, "writer":"관리자", "indate":"2021-08-27"} 
+		// ]
+		// 타입은 String인데 형식이 JSON포멧형식
+		// String타입이기 때문에 JSON을 받는 클라이언트는 JSON을 핸들링하는  API를 통해 해석할 수 있다
+		// 결과 보기 힘들면 JSON VIEWER 사용하면 된다~
+	}
+}
